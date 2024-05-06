95246417c719f9940a5767f36fdaabb55026ef12
Cap_of_Tea <xof.course.notx@gmail.com>
2024-05-06 23:02:00 +0300

    Weapons
    semi-working

--- Assets/Scripts/WeaponSurek.cs
+++ Assets/Scripts/WeaponSurek.cs
@@ -8,12 +8,6 @@ public class WeaponSurek : MonoBehaviour
     public Transform shotpos;
     public GameObject Bullet;
     
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     public void FireBullet()
     {
         Instantiate(Bullet, shotpos.transform.position, transform.rotation);
--- Assets/Scripts/weaponSwitch.cs
+++ Assets/Scripts/weaponSwitch.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class weaponSwitch : MonoBehaviour
 {
-    public class WeaponSwitch : MonoBehaviour
-{
+    /*
     public playerAttack meleeAttack; // Ссылка на скрипт атаки в ближнем бою
     public WeaponSurek surekAttack; // Ссылка на скрипт запуска снарядов
+    */
 
     public bool isMeleeMode = true; // Переменная для отслеживания текущего режима атаки
 
-    // Вызывается при нажатии клавиши для переключения режима атаки
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Tab)) // АРТУР это смена режима атаки
-        {
-            ToggleAttackMode();
-        }
-    }
-
     // Метод для переключения режима атаки
-    private void ToggleAttackMode()
+    public void ToggleAttackMode()
     {
         isMeleeMode = !isMeleeMode; // Инвертируем значение переменной
 
         // Включаем или выключаем соответствующие скрипты в зависимости от режима
+        /*
         meleeAttack.enabled = isMeleeMode;
         surekAttack.enabled = !isMeleeMode;
+        */
     }
-}
 
 }
