e9df811ef7ab5dabb6f8f5def05a5f01b8d2767b
DoctorNik <nikandr2018@gmail.com>
2024-05-11 01:33:02 +0300

    Смена кнопки атаки
    Если атака на дальней дистанции, то иконка отображается в виде лука.
    Если ближняя, то меч

--- Assets/Scripts/weaponSwitch.cs
+++ Assets/Scripts/weaponSwitch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class weaponSwitch : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class weaponSwitch : MonoBehaviour
     public playerAttack meleeAttack; // Ссылка на скрипт атаки в ближнем бою
     public WeaponSurek surekAttack; // Ссылка на скрипт запуска снарядов
     */
+    public Image attackIcon; // Ссылка на изображение иконки атаки в GUI
+    public Sprite meleeAttackIcon; // Иконка для ближнего боя
+    public Sprite rangedAttackIcon; // Иконка для дальнего боя
 
     public bool isMeleeMode = true; // Переменная для отслеживания текущего режима атаки
 
@@ -15,12 +19,20 @@ public class weaponSwitch : MonoBehaviour
     public void ToggleAttackMode()
     {
         isMeleeMode = !isMeleeMode; // Инвертируем значение переменной
-
+        UpdateAttackIcon();
         // Включаем или выключаем соответствующие скрипты в зависимости от режима
         /*
         meleeAttack.enabled = isMeleeMode;
         surekAttack.enabled = !isMeleeMode;
         */
+    }
+
+    private void UpdateAttackIcon()
+    {
+        if (attackIcon != null)
+        {
+            attackIcon.sprite = isMeleeMode ? meleeAttackIcon : rangedAttackIcon; // Устанавливаем нужную иконку в зависимости от режима
+        }
     }
 
 }
